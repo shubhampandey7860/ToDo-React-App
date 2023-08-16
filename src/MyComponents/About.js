08c64f6d52acd3c931b6f93b1907a89e7ddc7091
shubhampandey7860 <109323696+shubhampandey7860@users.noreply.github.com>
2023-08-16 23:51:41 +0530

    react-app

---
+++ src/MyComponents/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            A React todo application is a simple web app that allows you to create, read, update, and delete a list of tasks using React, a popular JavaScript library for building user interfaces. A React todo app typically consists of the following components:
+            <ul>
+                <li>
+                    A text input field where you can type a new task and add it to the list by pressing enter or clicking a button.
+                </li>
+                <li>A list of tasks that shows the task name, a checkbox to mark the task as completed, and a delete button to remove the task from the list.
+                </li>
+                <li>A filter option that lets you choose to see all tasks, only completed tasks, or only active tasks.
+                    There are many tutorials and examples on how to build a React todo app using different approaches and technologies</li>
+            </ul>
+        </div>
+    )
+}
